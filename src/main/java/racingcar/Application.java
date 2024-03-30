25ac3db3908a1a7b06a4fb8b5034b5778b94a6e9
JanooGwan <janoogwan39@gmail.com>
2024-03-30 18:21:43 +0900

    자동차 이름 & 시도 횟수 입력 받기, 자동차 객체 생성 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Scanner;
 
 class Car {
     int pos=0;
@@ -20,6 +21,18 @@ class Car {
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 ")
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+        System.out.println("이름은 쉼표(,) 기준으로 구분");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split(",");
+        Car[] cars = new Car[1000];
+
+        for(int i=0; i<arr.length; i++) cars[i]=new Car(arr[i]);
+
+        System.out.println("몇 회 시도하실 건가요?");
+        int n = sc.nextInt();
+
     }
 }
