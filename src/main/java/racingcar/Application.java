576bd41322a4c7156c072f9f25ea4d75e900a73b
JanooGwan <janoogwan39@gmail.com>
2024-03-30 17:55:30 +0900

    Car 클래스 생성(객체 생성, 전진, 현재 상태 출력 기능 구현)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
+class Car {
+    int pos=0;
+    String name;
+    public Car(String name) {
+        this.name=name;
+    }
+
+    public void move() {
+        int n=Randoms.pickNumberInRange(0,9);
+        if (n>=4) pos+=n;
+    }
+
+    public void result_print() {
+        System.out.println(this.name + " : " + this.pos);
+    }
+}
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 ")
     }
 }
