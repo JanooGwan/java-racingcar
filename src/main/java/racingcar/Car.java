a6726730f8b0731f943ab4c8550ea135acd80b68
JanooGwan <janoogwan39@gmail.com>
2025-03-18 20:33:53 +0900

    Car 클래스 작성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int location;
+
+    public Car(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    public void move() {
+        ++location;
+    }
+}
