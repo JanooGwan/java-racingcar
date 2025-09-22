7bbb90e0b894283c29ebaaa69bd38b99ddcbea48
JanooGwan <janoogwan39@gmail.com>
2025-09-22 14:19:04 +0900

    feat: InputView & OutputView 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,15 @@
 package racingcar.view;
 
-public class InputView {
+import java.util.Scanner;
 
+public class InputView {
+    public void getStringInput() {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+    }
+
+    public void getNumInput() {
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+    }
 }
