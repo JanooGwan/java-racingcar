a2c3577bf6e73071f44b2df48bcb1b233d784495
JanooGwan <janoogwan39@gmail.com>
2025-09-22 19:39:25 +0900

    refactor: CheckCarName 수정

--- src/main/java/racingcar/util/CheckCarName.java
+++ src/main/java/racingcar/util/CheckCarName.java
@@ -1,20 +1,26 @@
 package racingcar.util;
 
 public class CheckCarName {
-    private final int NAME_MAX_LENGTH = 5;
+    private static final int NAME_MAX_LENGTH = 5;
 
-    public void isNull(String name) {
+    public static void isNull(String name) {
         if(name == null)
             throw new IllegalArgumentException("이름에는 null 값이 들어갈 수 없습니다.");
     }
 
-    public void isBlank(String name) {
+    public static void isBlank(String name) {
         if(name.isBlank())
             throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
     }
 
-    public void isMoreThanMaxLength(String name) {
-        if(name.length() > 5)
+    public static void isMoreThanMaxLength(String name) {
+        if(name.length() > NAME_MAX_LENGTH)
             throw new IllegalArgumentException("이름은 5자를 넘을 수 없습니다.");
+    }
+
+    public static void checkName(String name) {
+        isNull(name);
+        isBlank(name);
+        isMoreThanMaxLength(name);
     }
 }
