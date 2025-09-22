d728091ff54fa1462f8d3f70ecee7a235677c68f
JanooGwan <janoogwan39@gmail.com>
2025-09-22 20:02:36 +0900

    feat: CheckTryCount 클래스 작성

--- src/main/java/racingcar/util/CheckTryCount.java
+++ src/main/java/racingcar/util/CheckTryCount.java
@@ -1,4 +1,8 @@
 package racingcar.util;
 
 public class CheckTryCount {
+    public static void isPositive(int tryCnt) {
+        if(tryCnt < 0)
+            throw new IllegalArgumentException("시도 횟수는 자연수여야 합니다.");
+    }
 }
