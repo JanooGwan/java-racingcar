0b2d3510d24e6a7d4f2729f03635af76f6f168f9
JanooGwan <janoogwan39@gmail.com>
2025-09-29 11:24:48 +0900

    refactor: InputView - getNumInput() 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,13 +9,4 @@ public class InputView {
     public String getStringInput() {
         return sc.nextLine();
     }
-
-    public int getNumInput() {
-        try {
-            return sc.nextInt();
-        }
-        catch (InputMismatchException e) {
-            throw new IllegalArgumentException("숫자 외 다른 형태의 값을 입력할 수 없습니다.");
-        }
-    }
 }
